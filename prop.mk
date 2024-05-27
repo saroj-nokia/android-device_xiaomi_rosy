a43e7a33452352e455204bd2346699fbfa2ce0b3
Saroj Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2024-05-27 08:10:33 +0545

    remove 'added some Dalvik props'

--- prop.mk
+++ prop.mk
@@ -130,15 +130,9 @@ persist.vendor.cne.feature=1
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.debug.coresight.config=stm-events
 
-# Dalvik
+# Dalvik dex2oat
 PRODUCT_PROPERTY_OVERRIDES += \
-dalvik.vm.dex2oat64.enabled=true \
-dalvik.vm.heapstartsize=16m \
-dalvik.vm.heapgrowthlimit=192m \
-dalvik.vm.heapsize=512m \
-dalvik.vm.heaptargetutilization=0.75 \
-dalvik.vm.heapminfree=2m \
-dalvik.vm.heapmaxfree=8m
+dalvik.vm.dex2oat64.enabled=true
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
